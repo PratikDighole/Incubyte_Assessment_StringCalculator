df2ee005610e007afdcde04ab1a56fd008fabc8e
Pratik Dighole <pratikdighole2019@gmail.com>
2022-09-08 00:24:53 +0530

    TestCase1 Passed

---
+++ string-calculator.js
@@ -0,0 +1,15 @@
+function StringCalculator() {
+
+    
+}
+
+
+StringCalculator.prototype.add = function(string_numbers) {
+    
+ if (string_numbers == '') {
+        return 0;
+    }
+     
+};
+
+module.exports=StringCalculator
