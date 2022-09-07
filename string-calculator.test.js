ff1cf858fde112c04cc1ebc0887378c7f835d92e
Pratik Dighole <pratikdighole2019@gmail.com>
2022-09-08 00:51:55 +0530

    TestCase4 Passed

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -18,4 +18,19 @@ describe("String Calculator",function(){
    it ( " should return the sum of the numbers if two are given " , function () {
     expect ( calculator.add ( '1,2' ) ) .toEqual(3) ;
    }) ;
+
+   it ( " should return the sum of an unknown amount of numbers " , function ( ) {
+    // creates a random array
+    randomArray = ( length , max ) => [ ... new Array ( length ) ]
+    .map ( ( ) => Math.round ( Math.random ( ) * max ) ) ;
+    // creates a random number between 1 and 100
+    randy = Math.floor ( ( Math.random ( ) * 100 ) + 1 ) ;
+    res= randomArray ( randy , randy ) ;
+    // sums up an array
+    sum=res.reduce ( ( pv , cv ) => pv + cv , 0 ) ;
+    // converts the array to a string
+    arg = res.join ( ) ;
+    expect ( calculator.add ( arg ) ) . toEqual ( sum ) ;
+  } ) ;
+           
 })
