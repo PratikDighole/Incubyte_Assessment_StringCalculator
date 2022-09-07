cdcbd92fc8ed3bb6139ce512c487d14c09957d5a
Pratik Dighole <pratikdighole2019@gmail.com>
2022-09-08 00:48:39 +0530

    TestCase2 & TestCase3 Passed

--- string-calculator.js
+++ string-calculator.js
@@ -9,7 +9,19 @@ StringCalculator.prototype.add = function(string_numbers) {
  if (string_numbers == '') {
         return 0;
     }
-     
+     else if(string_numbers===1){
+        return (parseInt(string_numbers));
+     }
+     else{
+            var res = string_numbers.split (',') ;
+            var total = 0 ;
+            var arrayLength = res.length ;
+              for ( var i = 0 ; i < res.length ; i ++ ) {
+                total = total + parseInt ( res [ i ] ) ;
+              }
+              return total ;
+     }
 };
 
 module.exports=StringCalculator
+
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -12,7 +12,10 @@ describe("String Calculator",function(){
     expect(calculator.add('')).toEqual(0)
    })
 
-
-
-
+   it("should return one when 1 is passed", function(){
+    expect(calculator.add(1)).toEqual(1)
+   })
+   it ( " should return the sum of the numbers if two are given " , function () {
+    expect ( calculator.add ( '1,2' ) ) .toEqual(3) ;
+   }) ;
 })
